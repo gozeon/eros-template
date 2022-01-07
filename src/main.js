d339a1138f7750302c03341de5f67219ac2808a4
goze <goze.qiu@gmail.com>
2022-01-07 11:22:42 +0800

    style: update theme

--- src/main.js
+++ src/main.js
@@ -6,12 +6,11 @@ import router from './router'
 import store from './store'
 
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-axios.defaults.baseURL = '/api/'
+axios.defaults.baseURL = process.env.VUE_APP_API
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
